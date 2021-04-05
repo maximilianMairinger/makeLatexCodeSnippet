002ac6dc40ecc62b6ca2aa54caf9c596e5327687
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-04-05 15:42:52 +0200

    set json config & repl app

--- replApp/init.ts
+++ replApp/init.ts
@@ -1,13 +1,29 @@
 import ajaon from "ajaon"
 import edom from "extended-dom"
+import download from "downloadar"
+
 
 
 document.body.css("background", "black")
 
 let { post, get } = ajaon();
 
-
-(async () => {
-  let res = await post("call")
-  console.log(res)
-})()
+const txt = document.createElement("textarea")
+txt.value = localStorage.val
+txt.css({width: 600, height: 400})
+txt.on("input", () => {
+  localStorage.val = txt.value
+})
+document.body.apd(txt);
+const btn = document.createElement("button")
+btn.innerText = "Lets go"
+document.body.apd(btn);
+btn.on("click", async () => {
+  
+  console.log("sending: ", txt.value)
+  let r = await post("renderPls", {
+    source: txt.value
+  }) as {id: string}
+  download("http://" + location.host + "/renders/" + r.id)
+  console.log("fileId", "http://" + location.host + "/renders/" + r.id)
+})
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,35 +7,31 @@ import uidHash from "uid-safe"
 import { promises as fs } from "fs"
 import slugify from "slugify"
 import slash from "slash"
+import clip from "clipboardy"
 
-/** Function that count occurrences of a substring in a string;
- * @param {String} string               The string
- * @param {String} subString            The sub string to search for
- * @param {Boolean} [allowOverlapping]  Optional. (Default:false)
- *
- * @author Vitim.us https://gist.github.com/victornpb/7736865
- * @see Unit Test https://jsfiddle.net/Victornpb/5axuh96u/
- * @see http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string/7924240#7924240
- */
- function occurrences(string, subString, allowOverlapping) {
 
-  string += "";
-  subString += "";
-  if (subString.length <= 0) return (string.length + 1);
-
-  var n = 0,
-      pos = 0,
-      step = allowOverlapping ? 1 : subString.length;
-
-  while (true) {
-      pos = string.indexOf(subString, pos);
-      if (pos >= 0) {
-          ++n;
-          pos += step;
-      } else break;
-  }
-  return n;
+const editorConfig = {
+  "editor.formatOnSave": false,
+  "editor.fontSize": 20,
+  "editor.tabSize": 2,
+  "editor.minimap.enabled": false,
+  "workbench.editor.openSideBySideDirection": "down",
+  "svelte.plugin.typescript.diagnostics.enable": false,
+  "javascript.autoClosingTags": false,
+  "typescript.autoClosingTags": false,
+  "typescript.tsserver.useSeparateSyntaxServer": false,
+  "workbench.colorTheme": "Atom One Dark",
+  "html.autoClosingTags": false,
+  "typescript.locale": "en",
+  "editor.wrappingIndent": "none",
+  "editor.autoIndent": false,
+  "editor.quickSuggestions": false,
+  "editor.autoClosingBrackets": false,
+  "editor.formatOnType": false,
+  "editor.acceptSuggestionOnEnter": "off",
+  "editor.wordWrap": "on"
 }
+
 
 function nowStr() {
   return new Date().toLocaleString("de-AT", { timeZone: "Europe/Vienna" })
@@ -58,15 +54,39 @@ async function constrIncFileHash(path: string, filename: string | (() => string)
 }
 
 
-const app = setup()
-
-app.post("/echo", (req, res) => {
-  res.send(req.body)
-});
 
 
 
 (async () => {
+  const app = setup()
+
+  const inRender = new Map()
+
+  app.post("/renderPls", async (req, res) => {
+    const r = await render(req.body.source, req.body.options)
+    res.send({id: r.id})
+    inRender.set(r.id, r.done)
+    r.done.then(() => {
+      inRender.delete(r.id)
+    })
+  });
+
+  app.get("/renders/:id", (req, res, continue_) => {
+    const renderDone = inRender.get(req.params.id)
+    if (renderDone !== undefined) {
+      renderDone.then(() => {
+        res.sendFile(`public/renders/${req.params.id}`)
+      })
+    }
+    else res.send({err: "Invalid id"})
+  })
+
+
+
+
+
+
+
   const tempHash = await constrIncFileHash("tmp", "screenshot_", ".png")
   const endHash = await constrIncFileHash("public/renders/", "", ".png")
 
@@ -80,189 +100,261 @@ app.post("/echo", (req, res) => {
     resolutionFactor: 6
   }
 
+
   const render = async (source: string, options: {lang?: string, theme?: "dark" | "light", numbers?: boolean, autoFormat?: boolean, resolutionFactor?: number} = {}) => {
-    console.log("render request at ", nowStr())
-    options = merge(defaultOptions, options) as any
-
-    console.log("starting");
-
-
-    const browser = await puppeteer.launch({ headless: false })
-    const page = await browser.newPage()
-
-    const activeElement = async () => await page.evaluateHandle(() => document.activeElement) as any
-    const type = async (text: string, ms?: number) => await (await activeElement()).type(text, ms ? {delay: ms} : undefined)
-    const openCmdPallet = async () => {
-      await page.keyboard.down('ControlLeft')
-      await page.keyboard.down('ShiftLeft')
-      await page.keyboard.press('P')
-      await page.keyboard.up('ShiftLeft')
-      await page.keyboard.up("ControlLeft")
-    }
-
-    const linesOfSource = occurrences(source, "\n", false) + 1
-
-
-    await page.setViewport({
-      width: 1920,
-      height: 980 + (20 * linesOfSource),
-      deviceScaleFactor: options.resolutionFactor
-    });
-    await page.goto('https://codesandbox.io/s/vanilla')
-
-    await page.waitForSelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > textarea")
-      
-      
-    await openCmdPallet()
-
-    await delay(200)
-
-    await type('new file')
-
-    await delay(3500)
-
-    await page.keyboard.press('Enter')
-
-    await delay(1500)
-
-    await type(source)
+    const [ tempFilename, endFilename ] = await Promise.all([tempHash(), endHash()])
+    console.log("render request at ", nowStr(), "filename: ", endFilename, "source: \n", source)
 
 
 
-    if (options.autoFormat) {
-      await page.keyboard.down('ControlLeft')
-      await page.keyboard.press('S')
-      await page.keyboard.up('ControlLeft')
-    }
-    else { 
-      await openCmdPallet()
-      await delay(200)
-      await type("file: save without formatting")
-      await delay(2000)
-      await page.keyboard.press('Enter')  
-    }
-    
+    const done = (async () => {
+      options = merge(defaultOptions, options) as any
 
 
-    await delay(500)
-
-    type(`/code.${options.lang}`)
-
-    await delay(500)
-    
-    await page.keyboard.press('Enter')
-    
-    await delay(1000)
-
-    
-
-    if (options.theme.toLowerCase().includes("light")) {
-      await openCmdPallet()
-      await delay(200)
-      await type("preferences: color theme")
-      await delay(1000)
-      await page.keyboard.press('Enter')
-      await delay(500)
-      await type("atom one light")
-      await delay(500)
-      await page.keyboard.press('Enter')
-    }
-
-    await delay(2000)
-
-    await openCmdPallet()
-
-    await delay(200)
-    await type("toggle word wrap")
-    await delay(1000)
-    await page.keyboard.press('Enter')
-    await delay(500)
-
-
-
-
-    await page.evaluate((options) => {
-      const linting = document.querySelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > div.monaco-scrollable-element.editor-scrollable.vs > div.lines-content.monaco-editor-background > div.view-overlays")
-      if (linting) linting.remove()
-
-      if (options.theme === "light") {
-        const numbers = document.querySelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > div.margin") as HTMLElement
-        numbers.style.backgroundColor = "white"
-
-        const mainTextBody = document.querySelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > div.monaco-scrollable-element.editor-scrollable.vs > div.lines-content.monaco-editor-background > div.view-lines") as HTMLElement
-        mainTextBody.style.backgroundColor = "white"
+      const browser = await puppeteer.launch({ headless: true })
+      const page = await browser.newPage()
+  
+      const activeElement = async () => await page.evaluateHandle(() => document.activeElement) as any
+      const type = async (text: string, ms?: number) => await (await activeElement()).type(text, ms ? {delay: ms} : undefined)
+      const openCmdPallet = async () => {
+        await page.keyboard.down('ControlLeft')
+        await page.keyboard.down('ShiftLeft')
+        await page.keyboard.press('P')
+        await page.keyboard.up('ShiftLeft')
+        await page.keyboard.up("ControlLeft")
       }
-    }, options)
 
-    await delay(500)
+      const openSettings = async () => {
+        await page.keyboard.down('ControlLeft')
+        await page.keyboard.press("Comma")
+        await page.keyboard.up("ControlLeft")
+        await delay(1500)
+      }
 
-    await page.keyboard.down("ControlLeft")
-    await page.keyboard.press("End")
-    await page.keyboard.up("ControlLeft")
+      const paste = async (text: string) => {
+        const before = await clip.read()
+        await clip.write(text)
+        await page.keyboard.down('Control');
+        await page.keyboard.down('Shift');
+        await page.keyboard.press('KeyV');
+        await page.keyboard.up('Control');
+        await page.keyboard.up('Shift');
+        await clip.write(before)
+      }
 
-    await delay(500)
+      const save = async () => {
+        await page.keyboard.down('ControlLeft')
+        await page.keyboard.press('S')
+        await page.keyboard.up('ControlLeft')
+      }
 
-    await type("\n\n\n")
 
-    await delay(500)
+  
+      
+      const linesOfSource = source.split("\n").length
+  
+  
+      await page.setViewport({
+        width: 1920,
+        height: 980 + (20 * linesOfSource),
+        deviceScaleFactor: options.resolutionFactor
+      });
+      await page.goto('https://codesandbox.io/s/vanilla')
+  
+      await page.waitForSelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > textarea")
 
-    await page.keyboard.down("ControlLeft")
-    await page.keyboard.press("End")
-    await page.keyboard.up("ControlLeft")
+      await delay(3000)
 
-    await delay(500)
 
-    const bounds = await page.evaluate((linesOfSource, numbers) => {
-      const lineBody = document.querySelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > div.monaco-scrollable-element.editor-scrollable.vs > div.lines-content.monaco-editor-background > div.view-lines")
-      const rect = lineBody.getBoundingClientRect()
-      const lines = lineBody.querySelectorAll("div > span") as NodeListOf<HTMLElement>
+      if (options.theme.toLowerCase().includes("light")) {
+        editorConfig["workbench.colorTheme"] = "Atom One Light"
+      }
 
-      let maxWidth = 0
-      lines.forEach((line) => {
-        if (maxWidth < line.offsetWidth) maxWidth = line.offsetWidth
+
+
+      await openSettings()
+      page.evaluate(() => {
+        (document.querySelector("#vscode-editor > div > div > div > div > div > div.split-view-container > div > div > div.title.tabs.show-file-icons > div.tabs-and-actions-container > div.editor-actions > div > div > ul > li:nth-child(1) > a") as HTMLElement).click()
       })
+      await delay(2500)
+      await page.keyboard.down('ControlLeft')
+      await page.keyboard.press('A')
+      await page.keyboard.up('ControlLeft')
+      await page.keyboard.press("Backspace")
+      await delay(100)
+      await paste(JSON.stringify(editorConfig))
+      await save()
+      await delay(500)
+      await openSettings()
+      await paste("Editor: Drag and drop")
+      await delay(1000)
+      await page.keyboard.press("ArrowDown")
+      await delay(1000)
+      await page.keyboard.press("Enter")
+      await delay(3000)
 
-      const firstLine = lineBody.querySelector("div")
 
-      let lineHeight = firstLine ? firstLine.offsetHeight : 20
+      await openCmdPallet()
+      await paste("view: close all editors")
+      await delay(1500)
+      await page.keyboard.press("Enter")
+      await delay(1000)
 
-      const numbersWidth = numbers ? (document.querySelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > div.margin") as HTMLElement).offsetWidth : 0
       
+        
+        
+      await openCmdPallet()
+  
+      await delay(200)
+  
+      await paste('new file')
+  
+      await delay(1000)
+  
+      await page.keyboard.press('Enter')
+  
+      await delay(500)
+
+      await paste(source)
+
+      await delay(500)
+  
       
-      
-      return { 
-        top: rect.top,
-        left: rect.left - numbersWidth,
-        width: maxWidth + numbersWidth,
-        height: linesOfSource * lineHeight
+  
+  
+  
+      if (options.autoFormat) {
+        await save()
       }
-    }, linesOfSource, options.numbers)
+      else { 
+        await openCmdPallet()
+        await delay(200)
+        await paste("file: save without formatting")
+        await delay(2000)
+        await page.keyboard.press('Enter')  
+      }
+      
+  
+  
+      await delay(500)
+  
+      paste(`/code.${options.lang}`)
+  
+      await delay(500)
+      
+      await page.keyboard.press('Enter')
+      
+      await delay(1000)
+  
+      
+  
+      
+  
+      await delay(2000)
 
-    console.log(bounds)
 
-    for (let k in bounds) {
-      bounds[k] = bounds[k] * options.resolutionFactor
+
+  
+
+  
+  
+  
+      await page.evaluate((options) => {
+        const linting = document.querySelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > div.monaco-scrollable-element.editor-scrollable > div.lines-content.monaco-editor-background > div.view-overlays")
+        if (linting) linting.remove()
+  
+        if (options.theme === "light") {
+          const numbers = document.querySelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > div.margin") as HTMLElement
+          numbers.style.backgroundColor = "white"
+  
+          const mainTextBody = document.querySelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > div.monaco-scrollable-element.editor-scrollable > div.lines-content.monaco-editor-background > div.view-lines") as HTMLElement
+          mainTextBody.style.backgroundColor = "white"
+        }
+      }, options)
+  
+      await delay(500)
+  
+      await page.keyboard.down("ControlLeft")
+      await page.keyboard.press("End")
+      await page.keyboard.up("ControlLeft")
+  
+      await delay(500)
+  
+      await type("\n\n\n")
+  
+      await delay(500)
+  
+      await page.keyboard.down("ControlLeft")
+      await page.keyboard.press("End")
+      await page.keyboard.up("ControlLeft")
+  
+      await delay(500)
+  
+      const bounds = await page.evaluate((linesOfSource, numbers) => {
+        const lineBody = document.querySelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > div.monaco-scrollable-element.editor-scrollable > div.lines-content.monaco-editor-background > div.view-lines")
+        const rect = lineBody.getBoundingClientRect()
+        const lines = lineBody.querySelectorAll("div > span") as NodeListOf<HTMLElement>
+  
+        let maxWidth = 0
+        lines.forEach((line) => {
+          if (maxWidth < line.offsetWidth) maxWidth = line.offsetWidth
+        })
+  
+        const firstLine = lineBody.querySelector("div")
+  
+        let lineHeight = firstLine ? firstLine.offsetHeight : 20
+  
+        const numbersWidth = numbers ? (document.querySelector("#workbench\\.editors\\.files\\.textFileEditor > div > div.overflow-guard > div.margin") as HTMLElement).offsetWidth : 0
+        
+        
+        
+        return { 
+          top: rect.top,
+          left: rect.left - numbersWidth,
+          width: maxWidth + numbersWidth,
+          height: linesOfSource * lineHeight
+        }
+      }, linesOfSource, options.numbers)
+  
+      console.log(bounds)
+  
+      for (let k in bounds) {
+        bounds[k] = bounds[k] * options.resolutionFactor
+      }
+  
+  
+  
+  
+      await delay(3000)
+  
+      
+  
+      await page.screenshot({path: tempFilename})
+      browser.close()
+  
+      
+  
+      console.log("cropping image", endFilename)
+      await sharp(tempFilename).extract(bounds).toFile(endFilename)
+      
+  
+  
+      console.log("done cropping image", endFilename)
+    })()
+    
+    
+    return {
+      id: endFilename.split("/").last,
+      done
     }
-
-
-
-
-    await delay(3000)
-
     
-    const tempFilename = await tempHash()
-
-    await page.screenshot({path: tempFilename})
-    browser.close()
-
-    const endFilename = await endHash()
-
-    console.log("cropping image", tempFilename)
-    await sharp(tempFilename).extract(bounds).toFile(endFilename)
+    
+    
     
 
+    
 
-    console.log("done cropping image")
+    
 
     
 
@@ -270,12 +362,13 @@ app.post("/echo", (req, res) => {
 
     
     
+
 
     
     
   }
 
-  render(`let me = "Hello!"; console.log(me)\n//was soll das\nimport abc from "./abc"\nconsole.log(abc())`, {autoFormat: false, numbers: false})
+    
 })()
 
 
