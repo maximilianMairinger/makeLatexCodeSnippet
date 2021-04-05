a89a3275b4af4fa7016c1f62480ffd9b0f7036a0
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2021-04-05 02:20:47 +0200

    lines bugfix

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,35 @@ import { promises as fs } from "fs"
 import slugify from "slugify"
 import slash from "slash"
 
+/** Function that count occurrences of a substring in a string;
+ * @param {String} string               The string
+ * @param {String} subString            The sub string to search for
+ * @param {Boolean} [allowOverlapping]  Optional. (Default:false)
+ *
+ * @author Vitim.us https://gist.github.com/victornpb/7736865
+ * @see Unit Test https://jsfiddle.net/Victornpb/5axuh96u/
+ * @see http://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string/7924240#7924240
+ */
+ function occurrences(string, subString, allowOverlapping) {
+
+  string += "";
+  subString += "";
+  if (subString.length <= 0) return (string.length + 1);
+
+  var n = 0,
+      pos = 0,
+      step = allowOverlapping ? 1 : subString.length;
+
+  while (true) {
+      pos = string.indexOf(subString, pos);
+      if (pos >= 0) {
+          ++n;
+          pos += step;
+      } else break;
+  }
+  return n;
+}
+
 function nowStr() {
   return new Date().toLocaleString("de-AT", { timeZone: "Europe/Vienna" })
 }
@@ -71,11 +100,8 @@ app.post("/echo", (req, res) => {
       await page.keyboard.up("ControlLeft")
     }
 
-    const linesOfSource = (() => {
-      const matches = source.match("\n")
-      if (matches === null) return 1
-      else return matches.length + 1
-    })()
+    const linesOfSource = occurrences(source, "\n", false) + 1
+
 
     await page.setViewport({
       width: 1920,
@@ -246,7 +272,7 @@ app.post("/echo", (req, res) => {
     
   }
 
-  render(`let me = "Hello!"; console.log(me)\n//was soll das\nimport abc from "./abc\nconsole.log(abc())"`, {autoFormat: false, numbers: false})
+  render(`let me = "Hello!"; console.log(me)\n//was soll das\nimport abc from "./abc"\nconsole.log(abc())`, {autoFormat: false, numbers: false})
 })()
 
 
